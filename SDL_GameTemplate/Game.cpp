9ec3918458304afcddb8820ee4f64aa4300be162
claudiuohr <97850603+claudiuohr@users.noreply.github.com>
2022-01-17 22:48:06 +0200

    Update Game.cpp

--- SDL_GameTemplate/Game.cpp
+++ SDL_GameTemplate/Game.cpp
@@ -115,28 +115,28 @@ void Game::update() const
 {
 	if (startFlag == false)//verificam daca a fost apasat o data pe start
 	{
-		mousepressedstart = mouse({ 344,232,311,60 }) ? true : false;
+		mousepressedstart = mouse({ 344,232,311,60 }) ? true : false;//verificam daca a fost apast cu click stanga dreptunghiul de start
 		if (mousepressedstart)
-			startFlag = true;
-		exitflag = mouse({ 392,370,216,60 }) ? true : false;
+			startFlag = true;//daca da,trecem la urmatorul ecran
+		exitflag = mouse({ 392,370,216,60 }) ? true : false;//verificam daca a fost apasat cu click stanga dreptunghiul exit
 		if (exitflag)
-			isRunning = 0;
+			isRunning = 0;//daca da, oprim jocul
 	}
 
 	if (characterChosen == false && startFlag == true)// verificam daca si ce caracter a ales
 	{
-		SDL_Rect first{ 190,352,128,96 };
+		SDL_Rect first{ 190,352,128,96 };// dreptunghiurile pentru cele trei caractere din care putem alege
 		SDL_Rect second{ 436,352,128,96 };
 		SDL_Rect third{ 682,352,128,96 };
 
-		Coordonate aux = mouse();
+		Coordonate aux = mouse();//punem in aux coordonatele locului in care s a apasat click stanga
 
-		if (aux.x > first.x && aux.x < first.x + first.w)
+		if (aux.x > first.x && aux.x < first.x + first.w)//in urmatoarele 3 for uri verificam daca a fost ales vreun caracter si care
 		{
 			if (aux.y > first.y && aux.y < first.y + first.h)
 			{
 				chooseCharacterflag = 1;
-				characterChosen = true;
+				characterChosen = true;//vom iesi din acest if si vom trece la urmatorul ecran
 			}
 		}
 
@@ -160,18 +160,18 @@ void Game::update() const
 	}
 	if (characterChosen == true && mapChosen == false)// verificam daca a ales mapa si care anume
 	{
-		SDL_Rect lvl1{ 380,135,239,60 };
+		SDL_Rect lvl1{ 380,135,239,60 };//coordonatele patratelor celor 3 nivele din care poate alege jucatorul
 		SDL_Rect lvl2{ 374,295,251,60 };
 		SDL_Rect lvl3{ 374,455,251,60 };
 
-		Coordonate aux = mouse();
+		Coordonate aux = mouse();//punem in aux coordonatele locului in care s a apasat click stanga
 
-		if (aux.x > lvl1.x && aux.x < lvl1.x + lvl1.w)
+		if (aux.x > lvl1.x && aux.x < lvl1.x + lvl1.w)//in urmatoarele 3 for uri verificam daca a fost ales vreun caracter si care
 		{
 			if (aux.y > lvl1.y && aux.y < lvl1.y + lvl1.h)
 			{
 				Map::mapindex = 1;
-				mapChosen = true;
+				mapChosen = true;//vom iesi din acest if si vom trece la urmatorul ecran
 			}
 		}
 
@@ -217,7 +217,7 @@ void Game::render() const
 					if (levelpassed == 1)//caz in care trece nivelul
 					{
 						levelPassedSoundEffect = 1;
-						switch (Map::mapindex)
+						switch (Map::mapindex)//afisarea ecranelor de nivel trecut in functie de numarul nivelului
 						{
 						case 1:
 
@@ -255,13 +255,13 @@ void Game::render() const
 					if (Player::coliziune == 1)//caz in care s a lovit
 					{
 						SDL_RenderClear(renderer);
-						TextureManager::Draw(gameover, { 0, 0, 1000, 800 }, { 0, 0, 1000, 800 });
+						TextureManager::Draw(gameover, { 0, 0, 1000, 800 }, { 0, 0, 1000, 800 });//afisam meniul de game over 
 						SDL_RenderPresent(renderer);
-						replayflag = mouse({ 303,370,394,60 }) ? true : false;
+						replayflag = mouse({ 303,370,394,60 }) ? true : false;//poate alege daca doreste sa reia nivelul de la inceput sau sa nu mai joace
 						exitflag = mouse({ 392,494,216,60 }) ? true : false;
-						if (exitflag)
+						if (exitflag)//daca alege sa nu mai joace ,jocul se va inchide
 							isRunning = 0;
-						if (replayflag == 1)
+						if (replayflag == 1)//daca doreste sa joace din nou va fi incepe nivelul de la pozitia initiala
 						{
 							Player::coliziune = 0;
 							player->destRect = { 0,0,32,24 };
@@ -273,14 +273,14 @@ void Game::render() const
 			else
 			{
 				SDL_RenderClear(renderer);
-				TextureManager::Draw(levels, { 0, 0, 1000, 800 }, { 0, 0, 1000, 800 });
+				TextureManager::Draw(levels, { 0, 0, 1000, 800 }, { 0, 0, 1000, 800 });//afisare meniu de a alege nivelele
 				SDL_RenderPresent(renderer);
 			}
 		}
 		else
 		{
 			SDL_RenderClear(renderer);
-			TextureManager::Draw(chooseCharacter, { 0, 0, 1000, 800 }, { 0, 0, 1000, 800 });
+			TextureManager::Draw(chooseCharacter, { 0, 0, 1000, 800 }, { 0, 0, 1000, 800 });//afisare meniu de caractere
 			Animation::draw(player->texture1, player->texture2, player->srcRect, { 190,352,128,96 },60);
 			Animation::draw(player->texture3, player->texture4, player->srcRect, { 436,352,128,96 },60);
 			Animation::draw(player->texture5, player->texture6, player->srcRect, { 682,352,128,96 },60);
@@ -289,7 +289,7 @@ void Game::render() const
 	}
 	else
 	{
-		SDL_RenderClear(renderer);
+		SDL_RenderClear(renderer);//afisare ecran start
 		TextureManager::Draw(start, { 0, 0, 1000, 800 }, { 0, 0, 1000, 800 });
 		SDL_RenderPresent(renderer);
 	}
@@ -311,7 +311,7 @@ bool Game::mouse(SDL_Rect temp)// returneaza daca mouse ul este apasat intr un a
 	Uint32 buttons;
 	int x, y;
 	SDL_PumpEvents();
-	buttons = SDL_GetMouseState(&x, &y);
+	buttons = SDL_GetMouseState(&x, &y);//primeste coordonatele locului in care s a apasat click stanga
 	
 	if ((buttons & SDL_BUTTON_LEFT) != 0)
 	{
@@ -340,25 +340,23 @@ Coordonate Game::mouse() // returneaza coordonatele punctului apasat de mouse
 	}
 }
 
-void Game::PlayorStopMusic()
+void Game::PlayorStopMusic()//opreste muzica daca este pornita si o porneste daca este oprita
 {
 	if (Game::musicflag == true)
 	{
-		if (Game::onoffmusic == true )
+		if (Game::onoffmusic == true )//verifica daca e pornita si o opreste
 		{
 			Mix_PauseMusic();
 			Game::musicflag = false;
 			Game::onoffmusic = false;
-			std::cout << 3;
 		}
 		else
 		{
-			if (Game::onoffmusic == false)
+			if (Game::onoffmusic == false)//verifica daca e oprita si o porneste
 			{
 				Mix_PlayMusic(backgroundSound, -1);
 				Game::musicflag = false;
 				Game::onoffmusic = true;
-				std::cout << 2;
 			}
 		}
 	}
