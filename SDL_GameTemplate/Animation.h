e6c80c2329692073812f1ad474e247ca87fda649
claudiuohr <97850603+claudiuohr@users.noreply.github.com>
2022-01-17 22:18:14 +0200

    Update Animation.h

--- SDL_GameTemplate/Animation.h
+++ SDL_GameTemplate/Animation.h
@@ -5,16 +5,16 @@
 
 struct Animation
 {
-	static void draw(SDL_Texture* texture1, SDL_Texture* texture2, SDL_Rect srcRect, SDL_Rect destRect,int speed)
+	static void draw(SDL_Texture* texture1, SDL_Texture* texture2, SDL_Rect srcRect, SDL_Rect destRect,int speed)//functie pentru a afisa o animatie
 	{
-		if (Player::animatie <= (speed/2))
+		if (Player::animatie <= (speed/2))//de atatea ori va afisa prima imagine plus cazul in care punem animatie pe 0 pentru a reseta efectul
 		{
 			Player::animatie += 1;
 			TextureManager::Draw(texture1, srcRect, destRect);
 		}
 		else
 		{
-			if (Player::animatie <= speed)
+			if (Player::animatie <= speed)//de atatea ori va afisa a doua imagine pentru ca afisarea celor 2 imagini sa fie egala si sa para mai realista
 			{
 				Player::animatie += 1;
 				TextureManager::Draw(texture2, srcRect, destRect);
